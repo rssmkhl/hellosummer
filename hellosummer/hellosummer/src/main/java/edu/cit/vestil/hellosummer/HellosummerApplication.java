6952fdb36de8c91b932cc5336c7784507479908f
rssmkhl <rossvestil@gmail.com>
2026-06-24 15:47:07 +0800

    Add files via upload

---
+++ hellosummer/hellosummer/src/main/java/edu/cit/vestil/hellosummer/HellosummerApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.vestil.hellosummer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosummerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosummerApplication.class, args);
+	}
+
+}
